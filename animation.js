ededd461e4b4c16addc037119b76a30d99e9a90a
nguyentanhueqn123 <nguyentanhuebc@gmail.com>
2024-03-18 02:03:05 +0700

    fix: fix bug not show animation bg

--- animation.js
+++ animation.js
@@ -74,4 +74,47 @@ particlesJS("particles-js", {
                         "mode":"push"
                     },
                     "resize":true
-                },"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+                },"modes":{
+                    "grab":{
+                        "distance":400,
+                        "line_linked":{
+                            "opacity":1
+                        }
+                    },
+                    "bubble":{
+                        "distance":400,
+                        "size":40,
+                        "duration":2,
+                        "opacity":8,
+                        "speed":3},
+                        "repulse":{
+                            "distance":200,
+                            "duration":0.4
+                        },
+                        "push":{
+                            "particles_nb":4
+                        },
+                        "remove":{
+                            "particles_nb":2
+                        }
+                    }
+                },
+                "retina_detect":true
+            }
+            
+);
+                // var count_particles, stats, update; 
+                // stats = new Stats; stats.setMode(0); 
+                // stats.domElement.style.position = 'absolute'; 
+                // stats.domElement.style.left = '0px'; 
+                // stats.domElement.style.top = '0px'; 
+                // document.body.appendChild(stats.domElement); 
+                // count_particles = document.querySelector('.js-count-particles'); 
+                // update = function() { 
+                //     stats.begin(); 
+                //     // stats.end(); 
+                //     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { 
+                //         count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; 
+                //     } 
+                //     requestAnimationFrame(update); 
+                // }; requestAnimationFrame(update);;
